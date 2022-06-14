be4e03915abab295e94ad193c666e9e15266f5e3
Nikolay Yaremko <mendokusee@gmail.com>
2022-06-14 15:15:00 +0200

    Benchmark for 4xASTC with HDR

---
+++ runthemill.py
@@ -0,0 +1,12 @@
+INPUT = 'input-long/'
+
+import os
+import subprocess
+
+for filename in os.listdir(INPUT):
+    f = os.path.join(INPUT, filename)
+    if os.path.isfile(f) and filename != '.DS_Store':
+        bashCommand = "bash ./test.sh %s %s" % (filename, INPUT)
+        process = subprocess.Popen(bashCommand.split(), stdout=subprocess.PIPE)
+        output, error = process.communicate()
+
---
+++ test.sh
@@ -0,0 +1,17 @@
+/opt/homebrew/opt/imagemagick\@6/bin/convert \
+	$2/$1 \
+	-resize 2048x2048\<^ \
+	-extent 2048x2048 output/2048-$1
+
+/opt/homebrew/opt/imagemagick\@6/bin/convert \
+	$2/$1 \
+	-resize 1024x1024\<^ \
+	-extent 1024x1024 output/1024-$1
+
+./astc/astcenc-sse4.1 -cH output/1024-$1 output/1024-$1.astc 6x6 -medium
+./astc/astcenc-sse4.1 -cH output/2048-$1 output/1024-$1.astc 6x6 -medium
+./astc/astcenc-sse4.1 -cH output/1024-$1 output/1024-$1.astc 6x6 -medium
+./astc/astcenc-sse4.1 -cH output/2048-$1 output/1024-$1.astc 6x6 -medium
+
+zip output/1024-$1.astc output/1024-$1.astc.zip
+zip output/2048-$1.astc output/2048-$1.astc.zip
